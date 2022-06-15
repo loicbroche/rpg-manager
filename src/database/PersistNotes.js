69dd51977e2b9221c7169c5a5a3e56a99e5f2330
Loïc BROCHE <58534333+loicbroche@users.noreply.github.com>
2022-06-15 12:45:54 +0200

    Restrict global and personnal notes to a scenario

--- src/database/PersistNotes.js
+++ src/database/PersistNotes.js
@@ -6,13 +6,14 @@ export const ALL_CHARACTERS_ID = "*";
 
 const onPersistError = (error, url, data) => {console.warn(`Erreur de persistance de notes (${url}) : ${error}`, JSON.stringify(data))}
 
-const updateNotes = (notes, characterId) => {
-    const url = DATA_MODEL.NOTES.name + '/' + characterId;
+const updateNotes = (notes, scenarioId, characterId) => {
+    const url = DATA_MODEL.NOTES.name + '/' + scenarioId + '/' + characterId;
     return gameDatabase.ref(url).set(notes, (error) => { if (error) {onPersistError(error, url, notes)} });
 }
 updateNotes.propTypes = {
     notes: PropTypes.arrayOf(PropTypes.string),
-    characterId: PropTypes.string.isRequired
+    scenarioId: PropTypes.string.isRequired,
+	characterId: PropTypes.string.isRequired
 }
 
 export { updateNotes };
